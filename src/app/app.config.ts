2296f37daf3ebe0676e409920d2d088c86e3a682
JustSergioPB <justsergioit.com>
2024-09-15 12:00:09 +0200

    main: Paths setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,7 @@ import {
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { TranslocoHttpLoader } from './transloco-loader';
+import { TranslocoHttpLoader } from '@i18n/transloco-loader';
 import { provideTransloco } from '@jsverse/transloco';
 
 export const appConfig: ApplicationConfig = {
