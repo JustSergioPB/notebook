2839b7061ed57abf78c35215e4c561b65c67ea12
JustSergioPB <justsergioit.com>
2024-09-15 11:49:47 +0200

    main: i18n setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslocoService, TranslocoModule } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslocoModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'notebook';
+  translocoService = inject(TranslocoService);
+
+  changeLang(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    const selectedLang = selectElement.value;
+    this.translocoService.setActiveLang(selectedLang);
+  }
 }
